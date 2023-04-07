08803e21613dc6f8996db6e13c678ae500e521e7
facuguns <facuaeros@gmail.com>
2023-04-07 16:18:48 -0300

    se agrega el proyecto

---
+++ index.js
@@ -0,0 +1,4 @@
+const temaOscuro = () => {
+    document.querySelector("body").setAttribute("data-bs-theme", "dark");
+    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+}
