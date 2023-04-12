99e8d91524e10c858def9f503c9339e1f993b43b
Facundo Rodriguez <89461012+facuaeros@users.noreply.github.com>
2023-04-12 00:53:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const switchButton = document.getElementById('switch');
+switchButton.addEventListener('click',()=>{
+    document.body.classList.toggle('dark');
+    switchButton.classList.toggle('active')
+})
+
+
