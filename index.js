3a6008dc782fd6d74ee21911a202ba2f91d0e903
Facundo Rodriguez <89461012+facuaeros@users.noreply.github.com>
2023-04-12 00:53:53 -0300

    Delete index.js

--- index.js
+++
@@ -1,4 +0,0 @@
-const temaOscuro = () => {
-    document.querySelector("body").setAttribute("data-bs-theme", "dark");
-    document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
-}
